ccc342f95907df6ff08fa71fda2f4d2fd0eb58ea
mohiyuddin <mohiyuddin.shaikh@tradofina.com>
2024-09-16 07:46:36 +0530

    initial project scope complete

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -5,10 +5,15 @@ import { useSelector } from "react-redux";
 import { CartItem } from "../store/slices/cartSlice";
 import { RootState } from "../store";
 import { formatToIndianCurrency } from "../utilities/commonUtilities";
+import { useNavigate } from "react-router-dom";
 
 type Props = {};
 
+const DISCOUNT_PRICE = 500;
+
 export default function Cart({}: Props) {
+  const navigate = useNavigate();
+
   const cartItems: CartItem[] = useSelector(
     (state: RootState) => state.cart.items
   );
@@ -17,6 +22,15 @@ export default function Cart({}: Props) {
     return items.reduce((total, item) => {
       return total + item.product.price * item.quantity;
     }, 0);
+  };
+
+  const calculateFinalPrice = (items: CartItem[]): string => {
+    const cartPrice = calculateTotalSum(items);
+    return formatToIndianCurrency(cartPrice + DISCOUNT_PRICE);
+  };
+
+  const handleGoShopping = () => {
+    navigate("/");
   };
 
   return (
@@ -29,32 +43,56 @@ export default function Cart({}: Props) {
             <span className="items">({cartItems?.length} items)</span>
           </div>
           <div className="listing">
-            {cartItems?.length > 0
-              ? cartItems.map((cartItem, index) => (
-                  <CartListCard cartItem={cartItem} />
-                ))
-              : null}
+            {cartItems?.length > 0 ? (
+              cartItems.map((cartItem, index) => (
+                <CartListCard cartItem={cartItem} />
+              ))
+            ) : (
+              <div className="noItems">
+                <div className="text">Seems like you need some shoes!</div>
+                <div className="button" onClick={handleGoShopping}>
+                  Go Shopping!
+                </div>
+              </div>
+            )}
           </div>
         </div>
         <div className="summary">
           <div className="payment">
             <div className="paymentTitle">Payment Summary</div>
-            <div className="subtotal">
-              <span className="title">
-                Subtotal ({cartItems?.length} items)
-              </span>
-              <span className="value">
-                ₹ {formatToIndianCurrency(calculateTotalSum(cartItems))}
-              </span>
-            </div>
-            <div className="shipping">
-              <span className="title">Shipping costs</span>
-              <span className="value">₹ 500</span>
-            </div>
-            <div className="discount">
-              <span className="title">Discount</span>
-              <span className="value">-</span>
-            </div>
+            {cartItems?.length > 0 ? (
+              <>
+                <div className="subtotal">
+                  <span className="title">
+                    Subtotal ({cartItems?.length} items)
+                  </span>
+                  <span className="value">
+                    ₹ {formatToIndianCurrency(calculateTotalSum(cartItems))}
+                  </span>
+                </div>
+                <div className="shipping">
+                  <span className="title">Shipping costs</span>
+                  <span className="value">₹ {DISCOUNT_PRICE}</span>
+                </div>
+                <div className="discount">
+                  <span className="title">Discount</span>
+                  <span className="value">-</span>
+                </div>
+                <div className="totalWrap">
+                  <div className="total">
+                    <span className="title">Total</span>
+                    <span className="value">
+                      ₹ {calculateFinalPrice(cartItems)}
+                    </span>
+                  </div>
+                  <div className="checkout">
+                    <button>Checkout</button>
+                  </div>
+                </div>
+              </>
+            ) : (
+              "No summary"
+            )}
           </div>
         </div>
       </div>
